f6608d1c50991d88a77116e79ce73424f5e71e30
moksh chadha <mokshchadha@192.168.1.4>
2023-03-01 21:49:14 +0530

    scaffolded the structure for the project along with cpp setup

---
+++ Readme.md
@@ -0,0 +1,1 @@
+## My competative programming jouner from March 1 2023
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+ #ifndef ONLINE_JUDGE
+        freopen("input_file.txt", "r", stdin);
+        freopen("output_file.txt", "w", stdout);
+#endif
+
+        string x;
+        cin>>x;
+
+        cout<<x<<" "<<"world";
+
+    return 0;
+
+}
