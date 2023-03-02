91507b8e9dc226de841ddfc63e524478e89d8662
moksh chadha <mokshchadha@192.168.1.4>
2023-03-02 08:22:13 +0530

    solved frequency count in binary search using both non optimal and optimal methods

--- binary_search/frequency_count.cpp
+++ binary_search/frequency_count.cpp
@@ -1,3 +1,8 @@
+// given an sorted array we have to find the frequency of a given element
+
+// first use binary search to find the element and then just check the right and left indexes
+
+
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -41,6 +46,63 @@ int frequencyCount(vector<int> arr, int key){
     return frequency;
 }
 
+int lowerBound(vector<int> arr, int key){
+   int start =0;
+   int end = arr.size()-1;
+   int ans=0;
+
+    while(start <= end){
+
+        int mid = (end+start) /2;
+
+        if(arr[mid] == key) {
+            end = mid-1;
+            ans = mid;
+        }
+        else if(arr[mid]>key) {
+            end = mid-1;
+        }
+        else if(arr[mid]<key){
+            start = mid+1;
+        }
+    };
+
+
+    return ans;
+}
+
+int upperBound(vector<int> arr, int key){
+    int start =0 ;
+    int end = arr.size();
+
+    int ans;
+
+    while(start<=end){
+        int mid = (start + end )/2;
+
+        if(arr[mid] == key){
+            ans = mid;
+            start = mid+1;
+        }
+        else if(arr[mid]>key){
+            end = mid-1;
+        }
+        else {
+            start = mid+1;
+        }
+    }
+
+    return ans;
+}
+
+int frequencyCountOptimal(vector<int> arr, int key){
+    int lower = lowerBound(arr, key);
+    int upper = upperBound(arr, key);
+    return upper-lower;
+}
+
+
+
 
 
 int main(){
@@ -65,10 +127,11 @@ vect.push_back(3);
 vect.push_back(4);
 vect.push_back(4);
 
-int key = 1;
+int key = 2;
 
 
 int frequency = frequencyCount(vect , key);
+frequencyCountOptimal(vect, key);
 cout<<"frequency count of "<<key<<" is "<<frequency;
 
 return 0;
