5a877329eaaa23265963505f3100af02e74a7e54
moksh chadha <mokshchadha@mokshs-MacBook-Pro.local>
2023-03-04 21:42:17 +0530

    roated search in binary search done

--- main.cpp
+++ main.cpp
@@ -2,46 +2,95 @@
 
 using namespace std;
 
-int findIdx(vector<int> arr, int key) {
+int findPivotIdx(vector<int> arr){
+    int i=0;
     int n = arr.size();
-    int current = (n /2);
 
-    while(current!=0){
-        if(arr[current] == key) return current;
-        if(current +1 <= n-1 && arr[current + 1] == key) return current+1;
+    for(int i=1;i<n;i++){
+        if(arr[i] < arr[i-1]) return i;
+    }
 
-        if(arr[current]>key) current = current/2;
-        else {
-            current = current *2;
+    return i;
+}
+
+int rotatedSearch(vector<int> arr, int element){
+
+    int pivot = findPivotIdx(arr);
+    cout<<"pivot "<<pivot<<endl;
+
+    // put the array in proper sorting 
+
+    int firstHalf = pivot-1;
+
+    int secondHalf = arr.size() - pivot;
+
+    vector<int> sorted;
+
+    for(int i=pivot; i<arr.size(); i++){
+        sorted.push_back(arr[i]);
+    }
+
+    for(int i=0; i< pivot; i++){
+        sorted.push_back(arr[i]);
+    }
+
+   
+   //implement binary search in sorted array
+    
+    int start = 0;
+    int end = arr.size();
+
+    while(start<=end){
+        int mid = ( start + end )/2;
+
+        if(sorted[mid] == element){
+            return (mid + pivot)%arr.size() ;
+        }
+        else if(sorted[mid]> element){
+            end = mid-1;
+        }
+        else if(sorted[mid]<element){
+            start = mid+1;
         }
     }
+ 
 
-   return 0;
+    return 0;
 }
+ 
 
-int frequencyCount(vector<int> arr, int key){
-
-    int idx = findIdx(arr, key);
+int rotatedSearchOptimal(vector<int> arr, int element){
     int n = arr.size();
+    int start =0;  int end =n-1;
+    while(start <= end){
 
-    int frequency=0;
-    int frontCursor = idx;
-    int backCursor = idx-1; // so that we dont count the found index twice 
-    
-    while(frontCursor < n && arr[frontCursor] == key) {
-        frontCursor++;
-        frequency++;
-    }
+        int mid = (start+end)/2;
 
-    while(backCursor>-1 && arr[backCursor]==key){
-        backCursor--;
-        frequency++;
+        if(arr[mid] == element) return mid;
+
+        //case if the element is on L1
+        if(a[start]<=a[mid]){
+            if(element> a[start] and element<a[mid]) {
+                end = mid -1;
+            }
+            else {
+
+                start = mid+1;
+
+            }
+        }
+        //case if the element is on L2
+        else {
+            if(element > a[mid] and a[end]>element){
+                start = mid+1;
+            }
+            else {
+                end = mid -1;
+            }
+        }
     }
-    
-    return frequency;
+    return -1;
 }
-
-
 
 int main(){
  #ifndef ONLINE_JUDGE
@@ -51,25 +100,21 @@ int main(){
 
 vector<int> vect;
   
-vect.push_back(0);
+vect.push_back(7);
+vect.push_back(9);
+vect.push_back(10);
 vect.push_back(1);
-vect.push_back(1);
-vect.push_back(1);
-vect.push_back(1);
-vect.push_back(1);
-vect.push_back(1);
-vect.push_back(2);
-vect.push_back(2);
 vect.push_back(2);
 vect.push_back(3);
 vect.push_back(4);
-vect.push_back(4);
+vect.push_back(5);
+vect.push_back(6);
 
-int key = 1;
+int element = 10;
 
 
-int frequency = frequencyCount(vect , key);
-cout<<"frequency count of "<<key<<" is "<<frequency;
+cout<<rotatedSearch(vect, element);
+
 
 return 0;
 
